8cef7d01a59f5e5c979855c2759e97b10d71b802
unknown <sebastian.zamora-u@labinfo.is.escuelaing.edu.co>
2023-10-19 18:58:30 -0500

    Avance Clase

---
+++ src/main/test/java/co/edu/escuelaing/cvds/lab8/SpringBootTest.java
@@ -0,0 +1,14 @@
+package co.edu.escuelaing.cvds.lab8;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootTest {
+
+	@Test
+	void validIfTheDBHasOneEmployeeTest() {
+		
+	}
+
+}
